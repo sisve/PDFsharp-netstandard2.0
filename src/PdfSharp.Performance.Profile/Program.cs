d3f0af017e05127fbe71e5892d9750fd1966aa18
Jamin Menter <jamin.menter@ayahealthcare.com>
2018-11-16 16:57:05 -0800

    add simple program for running performance analysis

---
+++ src/PdfSharp.Performance.Profile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using PdfSharp.Pdf.IO;
+
+namespace PdfSharp.Performance.Profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Reading {args.Length} files...");
+            foreach (var path in args)
+            {
+                if (File.Exists(path))
+                {
+                    Console.Write($"Reading {path}...");
+                    var pdf = PdfReader.OpenAsync(new FileStream(path, FileMode.Open), PdfDocumentOpenMode.Modify).Result;
+                    Console.WriteLine($" Complete! {pdf.PageCount} pages read.");
+                }
+                else
+                    Console.Error.WriteLine("File not found:  " + path);
+            }
+#if DEBUG
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
