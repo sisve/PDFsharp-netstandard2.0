a9ee2a891311a0a67e583eedc6321d4e5d0dafce
Jamin Menter <jamin.menter@ayahealthcare.com>
2019-01-08 17:59:22 -0800

    add some integration tests

---
+++ src/PdfSharp.Test.Integration/PdfCreationTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using PdfSharp.Drawing;
+using PdfSharp.Fonts;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace PdfSharp.Test.Integration
+{
+    public class PdfCreationTest
+    {
+        public PdfCreationTest()
+        {
+            if (Directory.Exists("temp"))
+                Directory.Delete("temp", true);
+            Directory.CreateDirectory("temp");
+        }
+
+        [Fact]
+        public void TestAES128Encryption()
+        {
+            var doc = new PdfDocument();
+            /* WARNING
+             * annotations won't work without setting a value for GlobalFontSettings.FontResolver, which isn't distributed with 
+             * PDFSharpCore; a naive implementation is at the bottom of this file...
+             */
+            var font = new XFont(GlobalFontSettings.DefaultFontName, 12, XFontStyle.Regular);
+            for (int i = 1; i <= 3; i++)
+            {
+                var page = doc.AddPage();
+                var gfx = XGraphics.FromPdfPage(page);
+                gfx.DrawString($"Page {i}", font, XBrushes.Black, 25, 25);
+            }
+            doc.SecuritySettings.DocumentSecurityLevel = Pdf.Security.PdfDocumentSecurityLevel.Encrypted128BitAes;
+            doc.SecuritySettings.UserPassword = "pass";
+            var path = Path.Combine("temp", "test.pdf");
+            doc.Save(path);
+            File.Exists(path).Should().BeTrue();
+        }
+    }
+}
---
+++ src/PdfSharp.Test.Integration/PdfReadingTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using PdfSharp.Pdf.IO;
+using System;
+using System.IO;
+using Xunit;
+
+namespace PdfSharp.Test.Integration
+{
+    public class PdfReadingTest
+    {
+        [Theory]
+        [InlineData("RC4-128 open password.pdf", "pass")] //Adobe 6.0
+        [InlineData("AES-256 open password.pdf", "pass")] //Adobe 7.0
+        [InlineData("AES-128 open password.pdf", "pass")] //Adobe X
+        [InlineData("test.pdf", "pass")] //Created by PdfSharp
+        public void TestPasswordProtectedDocument(string fileName, string openPassword)
+        {
+            var path = Path.Combine("data", fileName);
+            Assert.Throws<PdfReaderException>(() =>
+            {
+                PdfReader.Open(path);
+            });
+            var doc = PdfReader.Open(path, openPassword);
+            doc.PageCount.Should().Be(3);
+        }
+
+        [Theory]
+        [InlineData("3 pages.pdf")]
+        public void TestRegularDocument(string fileName)
+        {
+            var path = Path.Combine("data", fileName);
+            var doc = PdfReader.Open(path);
+            doc.PageCount.Should().Be(3);
+        }
+    }
+}
