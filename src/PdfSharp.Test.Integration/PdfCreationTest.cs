adc72ef2a59718ec8aa08e0e7bfe40b2efd197a5
Jamin Menter <jamin.menter@ayahealthcare.com>
2019-03-29 15:01:27 -0700

    add integration test @scasteran's PR

--- src/PdfSharp.Test.Integration/PdfCreationTest.cs
+++ src/PdfSharp.Test.Integration/PdfCreationTest.cs
@@ -2,6 +2,7 @@
 using PdfSharp.Drawing;
 using PdfSharp.Fonts;
 using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,23 +21,25 @@ namespace PdfSharp.Test.Integration
         [Fact]
         public void TestAES128Encryption()
         {
+            var pageCount = 3;
+            var password = "pass";
+
             var doc = new PdfDocument();
-            /* WARNING
-             * annotations won't work without setting a value for GlobalFontSettings.FontResolver, which isn't distributed with 
-             * PDFSharpCore; a naive implementation is at the bottom of this file...
-             */
             var font = new XFont(GlobalFontSettings.DefaultFontName, 12, XFontStyle.Regular);
-            for (int i = 1; i <= 3; i++)
+            for (int i = 1; i <= pageCount; i++)
             {
                 var page = doc.AddPage();
                 var gfx = XGraphics.FromPdfPage(page);
                 gfx.DrawString($"Page {i}", font, XBrushes.Black, 25, 25);
             }
             doc.SecuritySettings.DocumentSecurityLevel = Pdf.Security.PdfDocumentSecurityLevel.Encrypted128BitAes;
-            doc.SecuritySettings.UserPassword = "pass";
+            doc.SecuritySettings.UserPassword = password;
             var path = Path.Combine("temp", "test.pdf");
             doc.Save(path);
             File.Exists(path).Should().BeTrue();
+
+            var newDoc = PdfReader.Open(path, password, PdfDocumentOpenMode.InformationOnly);
+            newDoc.PageCount.Should().Be(pageCount);
         }
     }
 }
